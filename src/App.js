6d8f7d7187122ab226336e1df3a9084132812a55
tarnas14 <tarnas14@gmail.com>
2019-06-29 13:38:46 +0200

    first form works

--- src/App.js
+++ src/App.js
@@ -33,63 +33,83 @@ class App extends Component {
   }
 
   renderForm = () => {
-    return (<Formik
-      initialValues={{expense: '', howMuch: 0, category: '', fixed: false}}
-      onSubmit={(values, {setSubmitting, resetForm}) => {
-        // submit here
-      }}
-    >
-      {({
-        values,
-        handleChange,
-        handleSubmit,
-        isSubmitting,
-        setFieldValue,
-        errors,
-        touched,
-      }) => (
-        <Form onSubmit={handleSubmit}>
-          <Input
-            error={Boolean(false)}
-            fluid
-            id="expense"
-            label="wydatek"
-            onChange={handleChange}
-            placeholder="wydatek"
-            required
-            value={values.expense}
-          />
-          <Input
-            error={Boolean(false)}
-            fluid
-            id="howMuch"
-            label="kwota"
-            onChange={handleChange}
-            placeholder="kwota"
-            required
-            step={0.01}
-            type="number"
-            value={values.howMuch}
-          />
-          <Input
-            error={Boolean(false)}
-            fluid
-            id="category"
-            label="kategoria"
-            list="categories"
-            onChange={handleChange}
-            placeholder="kategoria"
-            required
-            value={values.category}
-          />
-          <datalist id="categories">
-            {CATEGORIES.map(category => (
-              <option key={category} value={category} />
-            ))}
-          </datalist>
-        </Form>
-      )}
-     </Formik>)
+    return (
+      <Formik
+        initialValues={{expense: '', howMuch: 0, category: '', fixed: false}}
+        onSubmit={(values, {setSubmitting, resetForm}) => {
+          resetForm()
+        }}
+      >
+        {({
+          values,
+          handleChange,
+          handleSubmit,
+          isSubmitting,
+          setFieldValue,
+          errors,
+          touched,
+        }) => (
+          <Form onSubmit={handleSubmit}>
+            <Input
+              error={Boolean(false)}
+              fluid
+              id="expense"
+              label="wydatek"
+              onChange={handleChange}
+              placeholder="wydatek"
+              required
+              value={values.expense}
+            />
+            <Input
+              error={Boolean(false)}
+              fluid
+              id="howMuch"
+              label="kwota"
+              onChange={handleChange}
+              placeholder="kwota"
+              required
+              step={0.01}
+              type="number"
+              value={values.howMuch}
+            />
+            <Input
+              error={Boolean(false)}
+              fluid
+              id="category"
+              label="kategoria"
+              list="categories"
+              onChange={handleChange}
+              placeholder="kategoria"
+              required
+              value={values.category}
+            />
+            <datalist id="categories">
+              {CATEGORIES.map(category => (
+                <option key={category} value={category} />
+              ))}
+            </datalist>
+            <Field>
+              <Checkbox
+                checked={values.fixed}
+                label="powtarza się"
+                onChange={(e, {checked}) => setFieldValue('fixed', checked)}
+              />
+            </Field>
+            <Field>
+              <Button
+                fluid
+                disabled={isSubmitting}
+                positive
+                size="huge"
+                type="submit"
+              >
+                OK
+              </Button>
+            </Field>
+          </Form>
+        )}
+      </Formik>
+    )
   }
 
   render() {
