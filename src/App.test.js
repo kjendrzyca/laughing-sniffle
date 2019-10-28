f885630d54606792a319568c2a3b894dd343401e
tarnas14 <tarnas14@gmail.com>
2019-10-28 20:09:44 +0100

    first test - no implementation required, green out of the box

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import {render} from '@testing-library/react'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import App from './App'
+
+it('should render with 15000 balance initially, regardless of API response', async () => {
+  // given
+  const expectedBalance = 15000
+
+  // when
+  const {getByText} = render(<App />)
+
+  // then
+  expect(getByText(expectedBalance.toString())).toBeInTheDocument()
+})
